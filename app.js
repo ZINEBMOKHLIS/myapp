7c70b1b36105d9d43e34d7a98e10ea92ad808432
zineb <zinebmokhlis15@gmail.com>
2026-07-05 09:26:23 +0000

    first deploy

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from Render" });
+});
+const port = process.env.PORT || 3000;
+app.listen(port,"0.0.0.0." () => {
+  console.log("Server running on port " +port);
+});
---
+++ server.js
@@ -0,0 +1,5 @@
+
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", () => {
+  console.log("Server running");
+});
