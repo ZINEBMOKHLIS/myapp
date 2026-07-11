cfcaeb7a794a89be74adf9fdc8e8ce9570a2b6b5
zineb <zinebmokhlis15@gmail.com>
2026-07-11 19:32:38 +0000

    deploymyapp

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from Render" });
+app.get('/', (req, res) => {
+  res.send('Hello Cloud Foundry 🚀');
 });
-const port = process.env.PORT || 3000;
-app.listen(port,"0.0.0.0.", () => {
-  console.log("Server running on port " +port);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from Render 🚀" });
+app.get('/', (req, res) => {
+  res.send('Hello Cloud Foundry 🚀');
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
